b864f689898b1204cb9b1cb5a2b0c61e944dbf99
Damien Degorre <ddegorre@chyro.tv>
2015-08-25 18:37:44 +0200

    Create Qlowd FtpadmBundle Module

---
+++ src/Qlowd/FtpadmBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Qlowd\FtpadmBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('QlowdFtpadmBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/Qlowd/FtpadmBundle/QlowdFtpadmBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Qlowd\FtpadmBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class QlowdFtpadmBundle extends Bundle
+{
+}
