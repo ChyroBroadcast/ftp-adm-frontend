cf013d03f98211288bbddfb07dc708da477e0107
Clercin Guillaume <gclercin@intellique.com>
2015-08-30 20:12:28 +0200

    Update script 'update-quota' to send alert via email
    See issue #2
    
    Update crontab configuration file
    
    Current version, send email to localhost
    
    This version of script is NOT YET TESTED

--- install/update-quota.pl
+++ install/update-quota.pl
@@ -5,46 +5,118 @@ use warnings;
 
 use DBD::mysql;
 use DBI;
+use Getopt::Long;
+use Net::SMTP;
 
 ##
 # Source for using mysql from perl
 # http://zetcode.com/db/mysqlperl/dbi/
 # http://www.cyberciti.biz/faq/how-to-access-mysql-database-using-perl/
+# http://www.perlmonks.org/?node_id=88222
 ##
 
-my $log_file = '/var/log/ftp-adm-frontend/update-quota.log';
-open( my $log_fd, '>>', $log_file )
-    or die "Failed to open log file because $@";
-select $log_fd;
+my $do_alert_quota  = 0;
+my $do_update_quota = 0;
+my $log_file        = '/var/log/ftp-adm-frontend/update-quota.log';
 
 my $host = 'localhost';
 my $db   = 'qlowd';
 my $user = 'qlowd';
 my $pass = 'spider77';
 
-my $dbh = DBI->connect( "DBI:mysql:$db:$host", $user, $pass )
-    or die "Failed to connect to database because $DBI::errstr";
-my $update = $dbh->prepare('UPDATE Customer SET used_space = ? WHERE path = ?')
-    or die "Failed to prepare query bacuse $dbh->errstr";
+GetOptions(
+    'alert'        => \$do_alert_quota,
+    'update_quota' => \$do_update_quota,
+    'log_file=s'   => \$log_file,
+);
+
+if ( $do_alert_quota and $do_update_quota ) {
+    print "Fatal error: you must choose one of these parameters in ('--alert', '--update_quota')\n";
+    exit 1;
+}
+
+if ( !$do_alert_quota and !$do_update_quota ) {
+    print "Fatal error: you must choose between '--alert' and '--update_quota' parameter\n";
+    exit 1;
+}
+
+open( my $log_fd, '>>', $log_file )
+    or die "Failed to open log file because $@";
+select $log_fd;
+
+my $dbh = DBI->connect( "DBI:mysql:$db:$host", $user, $pass );
+unless ($dbh) {
+	print "Failed to connect to database because $DBI::errstr";
+	exit 2;
+}
+
+my $email = $dbh->prepare('SELECT fullname, mail FROM User WHERE is_admin AND customer = (SELECT id FROM Customer WHERE path = \'/mnt/raid/\' LIMIT 1)');
+unless ($email) {
+	print "Failed to prepare query because $dbh->errstr";
+	exit 2;
+}
+
+my $update = $dbh->prepare('UPDATE Customer SET used_space = ? WHERE path = ?');
+unless ($update) {
+	print "Failed to prepare query because $dbh->errstr";
+	exit 2;
+}
 
 my @lines = qx!xfs_quota -x -c "df -N"!;
-die "Failed to run 'xfs_quota' because $@" if $?;
+if ($?) {
+	print "Failed to run 'xfs_quota' because $@";
+	exit 2;
+}
 
 my $nb_updated = 0;
 foreach (@lines) {
     chomp;
 
-    if (/^(.+?)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+%)\s+(.+)$/) {
-        my $used = $3 * 1024;
-        my $dir  = $6;
+    if (/^(.+?)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)%\s+(.+)$/) {
+        my $used     = $3 * 1024;
+		my $pct_used = $5;
+        my $dir      = $6;
 
-        if ( $update->execute( $used, $dir ) ) {
-            $nb_updated++;
-        }
-        else {
-            print
-                "Failed to update customer '$dir' with used_space='$used' because $update->errstr\n";
-        }
+		if ( $do_alert_quota and $pct_used >= 80 ) {
+			unless ( $email->execute($dir) ) {
+				print "Failed to get admin users from customer ($dir) because $email->errstr\n";
+				next;
+			}
+
+			unless ( $email->row > 0 ) {
+				print "No admin user found from customer ($dir)\n";
+				next;
+			}
+
+			my $user = '';
+			my @emails;
+
+			while ( my $row = $email->fetchrow_array ) {
+				$user .= ', ' if length $user > 0;
+				$user .= $row->[0];
+
+				push @emails, '"' . $row->[0] . '" <' . $row->[1] . '>';
+			}
+
+			my $message = "Dear $user\n\nYour ftp account's space is runnin low (used: $pct_used%).\n\nBest regards\nQlowd";
+
+			my $smtp = Net::SMTP->new('localhost');
+			$smtp->mail('Qlowd (no-reply) <no-replay@qlowd.io>');
+			$smtp->to(@emails, { SkipBad => 1 });
+			$smtp->cc('Qlowd support <support@qlowd.io>');
+			$smtp->data();
+			$smtp->datasend('From: Qlowd (no-reply) <no-reply@qlowd.io>' . "\r\nTo: " . join(', ', @emails) . "\r\nCC: " . 'Qlowd support <support@qlowd.io>' . "\r\n$message\r\n");
+			$smtp->dataend();
+			$smtp->quit();
+		}
+		elsif ($do_update_quota) {
+			if ( $update->execute( $used, $dir ) ) {
+				$nb_updated++;
+			}
+			else {
+				print "Failed to update customer '$dir' with used_space='$used' because $update->errstr\n";
+			}
+		}
     }
 }
 
